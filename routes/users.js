c101e9ef243c7b6b885ee56b877276e9c7f72151
Guilherme Devito <devito552@gmail.com>
2026-03-31 14:14:18 -0300

    criando CRUD para usuarios

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const express = require('express');
 const routes = express.Router();
 const db = require('../db');
 
+//CRUD - Create, Read, Update, Delete
 //Get em usuarios
 routes.get('/', (req, res) => {
-  db.query('SELECT * FROM users', (err, results) => {
+  db.query('SELECT * FROM usuarios', (err, results) => {
     if (err) {
       res.status(500).json({ error: 'Erro ao buscar usuários' });
     } else {
@@ -12,5 +13,59 @@ routes.get('/', (req, res) => {
     }
   });
 });
+
+//Criar um novo usuario
+routes.post('/create', (req, res) => {
+  const { nome, email, senha } = req.body;
+  db.query('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senha], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Erro ao criar usuário' });
+    } else {
+      res.status(201).json({ id: results.insertId, nome, email });
+    }
+  });
+});
+
+//editar um usuario
+routes.put('/edit/:id', (req, res) => {
+  const { id } = req.params;
+  const { nome, email } = req.body;
+  db.query('UPDATE usuarios SET nome = ?, email = ? WHERE id = ?', [nome, email, id], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Erro ao atualizar usuário' });
+    } else {
+      res.json({ id, nome, email });
+    }
+  });
+});
+
+//deletar um usuario
+routes.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;  
+  db.query('DELETE FROM usuarios WHERE id = ?', [id], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Erro ao deletar usuário' });
+    } else {
+      res.json({ message: 'Usuário deletado com sucesso' });
+    }
+  });
+});
+
+//Buscar um usuario por id
+routes.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('SELECT * FROM usuarios WHERE id = ?', [id], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Erro ao buscar usuário' });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ error: 'Usuário não encontrado' });
+      } else {
+        res.json(results[0]);
+      }
+    }
+  });
+});
+
 
 module.exports = routes;
