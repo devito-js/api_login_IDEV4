8c59fb142118dff4fbaca14e051c627a9d3043e9
Guilherme Devito <devito552@gmail.com>
2026-04-02 16:59:20 -0300

    add corredores

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ app.use(express.json());
 
 //Rotas
 const userRoutes = require('./routes/users');
+const corredorRoutes = require ('./routes/corredores');
 app.use ('/users', userRoutes);
+app.use ('/corredores', corredorRoutes);
+
 
 
 module.exports = app;
